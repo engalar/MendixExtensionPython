9b468528aa3fe2b3ddac2c20353daa6b2362551c
engalar <engalar@gmail.com>
2025-08-05 17:58:36 +0800

    wip

--- setup.py
+++ setup.py
@@ -5,11 +5,17 @@ setup(
     version="1.2.2",
     packages=find_packages(),
     namespace_packages=['pymx'],
-    python_requires='>=3.6',
+    python_requires='>=3.11',
     install_requires=[
-        'starlette>=0.20.0',
-        'uvicorn>=0.15.0',
-        'requests>=2.25.0',
+        'starlette>=0.27.0',
+        'uvicorn>=0.23.2',
+        'requests>=2.31.0',
+        'pythonnet==3.0.5',
+        'anyio==4.9.0',
+        "pydantic>=2.11.1",
+        "typing_extensions==4.13.0",
+        "mcp==1.12.2",
+        "sse_starlette==1.6.5",
     ],
     extras_require={
         'dev': [
